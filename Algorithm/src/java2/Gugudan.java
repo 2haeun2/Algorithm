076602fa7893c451113c8713060ab999e63066f5
haeun <nueah2@gmail.com>
2022-04-19 17:05:29 +0900

    구구단

---
+++ Algorithm/src/java2/Gugudan.java
@@ -0,0 +1,13 @@
+package java2;
+
+public class Gugudan {
+	public static void main(String[] args) { 
+		for(int i = 2; i < 10; i++) { //´Ü
+			for(int j=1; j < 10; j++) {  //°öÇÏ´Â ¼ö
+				System.out.println(i + " * " + j + " = " + i * j); 
+				
+			} System.out.println();//´Ü¼ø ÁÙ ¹Ù²Þ
+		} 
+	}	
+}
+
